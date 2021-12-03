db8d9f780020718ef0912810a4366cc87b54967c
nnfp <nnfpup@gmail.com>
2021-12-02 16:04:47 -0800

    nnfu instrument + audio

--- client/src/instruments/nnfu.tsx
+++ client/src/instruments/nnfu.tsx
@@ -1,176 +1,73 @@
-// 3rd party library imports
-import * as Tone from 'tone';
-import classNames from 'classnames';
-import { List, Range } from 'immutable';
-import React from 'react';
+import { Instrument } from "../Instruments";
+import { useState } from "react/";
+import * as Tone from "tone";
 
-// project imports
-import { Instrument, InstrumentProps } from '../Instruments';
-
-/** ------------------------------------------------------------------------ **
- * Contains implementation of components for Piano.
- ** ------------------------------------------------------------------------ */
-
-interface PuddingKeyProps {
-  note: string; // C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B
-  duration?: string;
-  synth?: Tone.Synth; // Contains library code for making sound
-  minor?: boolean; // True if minor key, false if major key
-  octave: number;
-  index: number; // octave + index together give a location for the piano key
-}
-
-export function PuddingKey({
-  note,
-  synth,
-  minor,
-  index,
-}: PuddingKeyProps): JSX.Element {
-  /**
-   * This React component corresponds to either a major or minor key in the piano.
-   * See `PuddingKeyWithoutJSX` for the React component without JSX.
-   */
-  return (
-    // Observations:
-    // 1. The JSX refers to the HTML-looking syntax within TypeScript.
-    // 2. The JSX will be **transpiled** into the corresponding `React.createElement` library call.
-    // 3. The curly braces `{` and `}` should remind you of string interpolation.
-    <div
-      onMouseDown={() => synth?.triggerAttack(`${note}`)} // Question: what is `onMouseDown`?
-      onMouseUp={() => synth?.triggerRelease('+0.25')} // Question: what is `onMouseUp`?
-      className={classNames('ba pointer absolute dim', {
-        'blue bg-pink h4': !minor, 
-      })}
-      style={{
-        // CSS
-        top: 0,
-        left: `${index * 3.5}rem`,
-        width: 50,
-        height: 50,
-        borderRadius: 150/2,
-        // border: 20
-      }}
-    ></div>
-  );
-}
-
-// eslint-disable-next-line
-function PuddingKeyWithoutJSX({
-  note,
-  synth,
-  minor,
-  index,
-}: PuddingKeyProps): JSX.Element {
-  /**
-   * This React component for pedagogical purposes.
-   * See `PuddingKey` for the React component with JSX (JavaScript XML).
-   */
-  return React.createElement(
-    'div',
-    {
-      onMouseDown: () => synth?.triggerAttack(`${note}`),
-      onMouseUp: () => synth?.triggerRelease('+0.25'),
-      className: classNames('ba pointer absolute dim', {
-        'bg-black black h3': minor,
-        'black bg-white h4': !minor,
-      }),
-      style: {
-        top: 0,
-        left: `${index * 2}rem`,
-        zIndex: minor ? 1 : 0,
-        width: minor ? '1.5rem' : '2rem',
-        marginLeft: minor ? '0.25rem' : 0,
+function MeepSound(): JSX.Element {
+  const [meeps]: any = useState(
+    new Tone.Sampler({
+      urls: {
+        C1: "../meeps/Bard.meepSpawn01.ogg",
+        D1: "../meeps/Bard.meepSpawn02.ogg",
+        E1: "../meeps/Bard.meepSpawn03.ogg",
+        F1: "../meeps/Bard.meepSpawn04.ogg",
+        G1: "../meeps/Bard.meepSpawn05.ogg",
+        A1: "../meeps/Bard.meepSpawn06.ogg",
+        B1: "../meeps/Bard.meepSpawn07.ogg",
+        D2: "../meeps/Bard.meepSpawn08.ogg",
+        E2: "../meeps/Bard.meepSpawn09.ogg"
       },
-    },
-    [],
+    }).toDestination()
   );
-}
-
-function PuddingType({ title, onClick, active }: any): JSX.Element {
-  return (
-    <div
-      onClick={onClick}
-      className={classNames('dim pointer ph2 pv1 ba mr2 br1 fw7 bw1', {
-        'b--black black': active,
-        'gray b--light-gray': !active,
-      })}
-    >
-      {title}
-    </div>
-  );
-}
-
-function Pudding({ synth, setSynth }: InstrumentProps): JSX.Element {
-  const keys = List([
-    { note: 'C', idx: 0 },
-    { note: 'Db', idx: 1 },
-    { note: 'D', idx: 2 },
-    { note: 'Eb', idx: 3 },
-    { note: 'E', idx: 4 },
-    { note: 'F', idx: 5 },
-    { note: 'Gb', idx: 6 },
-    { note: 'G', idx: 7 },
-    { note: 'Ab', idx: 8 },
-    { note: 'A', idx: 9 },
-    { note: 'Bb', idx: 10 },
-    { note: 'B', idx: 11 },
-  ]);
-
-  const setOscillator = (newType: Tone.ToneOscillatorType) => {
-    setSynth(oldSynth => {
-      oldSynth.disconnect();
-
-      return new Tone.MembraneSynth({
-        oscillator: { type: newType } as Tone.OmniOscillatorOptions,
-      }).toDestination();
-    });
+  
+  const meeps_sound = () => {
+    meeps.triggerAttackRelease(["C1"],3.0);
+  };
+  const meeps_sound1 = () => {
+    meeps.triggerAttackRelease(["D1"],3.0);
+  };
+  const meeps_sound2 = () => {
+    meeps.triggerAttackRelease(["E1"],3.0);
+  };
+  const meeps_sound3 = () => {
+    meeps.triggerAttackRelease(["F1"],3.0);
+  };
+  const meeps_sound4 = () => {
+    meeps.triggerAttackRelease(["G1"],3.0);
+  };
+  const meeps_sound5 = () => {
+    meeps.triggerAttackRelease(["A1"],3.0);
+  };
+  const meeps_sound6 = () => {
+    meeps.triggerAttackRelease(["B1"],3.0);
+  };
+  const meeps_sound7 = () => {
+    meeps.triggerAttackRelease(["D2"],3.0);
+  };
+  const meeps_sound8 = () => {
+    meeps.triggerAttackRelease(["E2"],3.0);
   };
 
-  const oscillators: List<OscillatorType> = List([
-    'sine',
-    'sawtooth',
-    'square',
-    'triangle',
-    'fmsine',
-    'fmsawtooth',
-    'fmtriangle',
-    'amsine',
-    'amsawtooth',
-    'amtriangle',
-  ]) as List<OscillatorType>;
-
   return (
-    <div className="pv4">
-      <div className="relative dib h4 w-100 ml4">
-        {Range(2, 7).map(octave =>
-          keys.map(key => {
-            const isMinor = key.note.indexOf('b') !== -1;
-            const note = `${key.note}${octave}`;
-            return (
-              <PuddingKey
-                key={note} //react key
-                note={note}
-                synth={synth}
-                minor={isMinor}
-                octave={octave}
-                index={(octave) + key.idx}
-              />
-            );
-          }),
-        )}
+    <div>
+      <div style={{
+      marginLeft:"300px",
+      outline: "none"
+    }}>
+      <button style={{
+        width:"100px", outline: "none"}}><img src="https://i.imgur.com/o3AjnnJ.png" alt="base" onClick={meeps_sound} /></button>
+      <button style={{width:"100px"}}><img src="https://i.imgur.com/uJZL5eX.png" alt="base" onClick={meeps_sound1} /></button>
+      <button style={{width:"100px"}}><img src="https://i.imgur.com/EizO6Vh.png" alt="base" onClick={meeps_sound2} /></button>
+      <button style={{width:"100px"}}><img src="https://i.imgur.com/Oek4j1B.png" alt="base" onClick={meeps_sound3} /></button>
+      <button style={{width:"100px"}}><img src="https://i.imgur.com/jlFML2f.png" alt="base" onClick={meeps_sound4} /></button>
       </div>
-      <div className={'pl4 pt4 flex'}>
-        {oscillators.map(o => (
-          <PuddingType
-            key={o}
-            title={o}
-            onClick={() => setOscillator(o)}
-            active={synth?.oscillator.type === o}
-          />
-        ))}
+      <div style={{
+      marginLeft:"350px"
+    }}>      <button style={{width:"100px"}}><img src="https://i.imgur.com/HdMCqzw.png" alt="base" onClick={meeps_sound5} /></button>
+    <button style={{width:"100px"}}><img src="https://i.imgur.com/70WvKf1.png" alt="base" onClick={meeps_sound6} /></button>
+    <button style={{width:"100px"}}><img src="https://i.imgur.com/Mm2jaJV.png" alt="base" onClick={meeps_sound7} /></button>
+    <button style={{width:"100px"}}><img src="https://i.imgur.com/eWJfgdX.png" alt="base" onClick={meeps_sound8} /></button></div>
       </div>
-    </div>
   );
 }
 
-export const nnfuInstrument = new Instrument('nnfu', Pudding);
+export const nnfuInstrument = new Instrument('nnfu', MeepSound);
