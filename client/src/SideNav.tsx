fd85ae70c87f56dccda569eb0d68581cb05e395e
ngaiansel <70356894+ngaiansel@users.noreply.github.com>
2021-12-05 16:12:52 -0800

    SideNav display <SONG> - <ARTIST>

--- client/src/SideNav.tsx
+++ client/src/SideNav.tsx
@@ -115,7 +115,7 @@ function Songs({ state, dispatch }: SideNavProps): JSX.Element {
           }
         >
           <Music20 className="mr1" />
-          {song.get('songTitle')}
+          {song.get('songTitle') + " -"}  {song.get('artist')}
         </div>
       ))}
     </Section>
@@ -126,7 +126,7 @@ export function SideNav({ state, dispatch }: SideNavProps): JSX.Element {
   return (
     <div className="absolute top-0 left-0 bottom-0 w5 z-1 shadow-1 bg-white flex flex-column">
       <div className="h3 fw7 f5 flex items-center pl3 bb b--light-gray">
-        Nameless App
+        TUTUTONES
       </div>
       <div className="flex-auto">
         <Instruments state={state} dispatch={dispatch} />
